f74d942ef59532919c2feaf4c2149cf697d3e92e
6604062630421 <144242741+6604062630421@users.noreply.github.com>
2025-03-16 14:48:06 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router'
 import ScrollTop from './assets/component/ScrollTop'
-import Navbar from './assets/component/Navbar'
 import Home from './assets/page/Home'
 import NNdata from './assets/page/NNdata'
 import MLdata from './assets/page/MLdata'
+import Navbar from './assets/component/Navbar'
 function App() {
 
   return (
